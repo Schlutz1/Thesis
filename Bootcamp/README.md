c1b131776492a177ae4a61af1d2ac67cb6f8257d
Max Schultz <max.schultz1@gmail.com>
2017-09-10 22:59:12 +1000

    Edited to allow csvs to be added

--- Bootcamp/README.md
+++ Bootcamp/README.md
@@ -1,17 +1,23 @@
 # Framework for bootcamp
 
 ## main.py
-  * contains model and
+  * This is the main framework for the Neural network
+  * Optimisable hyperparameter is the number of hidden layers (n_hidden)
 
 ## loadData.py
   * loads standardised data set for testing different frameworks
-  * returns x_train, x_test, y_train, y_test
+  * can use sklearn standard data sets http://scikit-learn.org/stable/datasets/index.html
+  * returns x_train, x_test, y_train, y_test, n_classes, n_dimensionality
 
 ## gaussianProcess.py
   * performs gaussian process on given sample set
+  * ideally define your process/algo in this file
+  * returns y_mean, y_std
 
 ## surrogateFunction.py
   * returns maximum value of ei based on surrogate function
+  * ideally define your surrogate function in this file
+  * returns next_parameter
 
 ## plotProcess.py
   * visualises algorithims hyperparameter optimisation process
--- Bootcamp/loadData.py
+++ Bootcamp/loadData.py
@@ -1,31 +1,65 @@
 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 from sklearn import datasets
 
 from neupy import environment
+
+import pandas as pd
 import numpy as np
 
-def load_data() :
+import sys
+
+def load_data(path_to_file) :
+
   environment.reproducible()
 
-  dataset = datasets.load_digits()
-  n_samples = dataset.target.size
-  n_dimensionality = dataset.data.shape[1] #10 digits, 3 iris
+  #take str args, if none load default trial set
+  if path_to_file == None :
 
-  n_classes = []
-  for counter in dataset.target :
-      if counter not in n_classes :
-        n_classes.append(counter)
-  n_classes = max(n_classes) + 1
+    print("Loading sklearn dataset")
+    dataset = datasets.load_digits()
+    n_samples = dataset.target.size
+    n_dimensionality = dataset.data.shape[1] #gives input dimensions
 
-  # One-hot encoder
-  target = np.zeros((n_samples, n_classes))
-  target[np.arange(n_samples), dataset.target] = 1
+    n_classes = []
+    for counter in dataset.target :
+        if counter not in n_classes :
+          n_classes.append(counter)
+    n_classes = max(n_classes) + 1 #gives output dimensions
 
-  x_train, x_test, y_train, y_test = train_test_split(
-      dataset.data, target, train_size=0.7
-  )
+    # One-hot encoder
+    target = np.zeros((n_samples, n_classes))
+    target[np.arange(n_samples), dataset.target] = 1
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        dataset.data, target, train_size=0.7
+    )
+
+  #import data form custom file
+  else :
+
+    dataset = pd.read_csv(path_to_file)
+    data, target_raw = dataset.iloc[:,:-1], dataset.iloc[:,-1]
+    n_samples = dataset.shape[0]
+    n_dimensionality = data.shape[1]
+
+    # integer encode
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(target_raw)
+    # binary encode
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    target = onehot_encoder.fit_transform(integer_encoded)
+
+    n_classes = target.shape[1]
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        data, target, train_size=0.7
+    )
+
   return x_train, x_test, y_train, y_test, n_classes, n_dimensionality
 
 #if __name__ == '__main__':
-  #load_data()
+  #load_data( str(sys.argv[1]) )
