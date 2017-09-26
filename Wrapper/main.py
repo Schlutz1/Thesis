1844b6a7b53550ba97b79a7afe44f5629ea81689
kay-wong <kayyenwong@gmail.com>
2017-09-26 18:54:38 +1000

    Make scoreModel class with hyp_opt method

--- Wrapper/main.py
+++ Wrapper/main.py
@@ -28,29 +28,35 @@ def load_labels(filename):
     # done!
     return data
 
-# optimizae
-# p: run in parallel?
-def optimize(p, n_evals, **kwargs):
-    if p:
-        return 0
-    else:
-        return optimize_optunity(n_evals, **kwargs)
 
-def optimize_optunity(n_evals, **kwargs):
-    return optunity.maximize(score_model, num_evals=n_evals, **kwargs)
+class scoreModel:
+    def __init__(self, model, scoring_method):
+        self.model = model
+        self.scoring_method = scoring_method    #str of name of scoring method to use
+        
+    def score_model(self, x_train, y_train, x_test, y_test, **params):
+        model_inst = self.model(**params)
+        model_inst.fit(x_train, y_train)
+        decision_vals = model_inst.predict(x_test)
+        scoring = getattr(sklearn.metrics, self.scoring_method)
+        score = scoring(y_test, decision_vals)
+        return score
+
+    def hyp_opt_optunity(self, data, labels, p, n_evals, **kwargs):
+        if p:
+            return 0
+        else:
+            return self.optimize_optunity(n_evals, data, labels, **kwargs)
+    
+    def optimize_optunity(self, n_evals, data, labels, **kwargs):
+        cv_decorator = optunity.cross_validated(x=data, y=labels, num_folds=5)
+        scoreModel = cv_decorator(self.score_model)
+        return optunity.maximize(scoreModel, num_evals=n_evals, **kwargs)
+
+
 
 data = load_csv("abalone.data")
 labels = load_labels("abalone.labels")
 
-@optunity.cross_validated(x=data, y=labels, num_folds=2, num_iter=2)
-def score_model(x_train, y_train, x_test, y_test, **params):
-    model_inst = model(**params)
-    model_inst.fit(x_train, y_train)
-    decision_vals = model_inst.predict(x_test)
-    scoring = getattr(sklearn.metrics, scoring_method)
-    score = scoring(y_test, decision_vals)
-    return score
-
-model = SVC
-scoring_method = "accuracy_score"
-optimize(False, 5, C=[5, 7], gamma=[0, 1])
+model= scoreModel(SVC, 'accuracy_score')
+model.hyp_opt_optunity(data, labels, False, 5, C=[5, 7], gamma=[0, 1])
